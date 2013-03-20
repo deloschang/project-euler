a96fb6d43ac4289d93697f6e826621c13b6b2fe2
Delos Chang <lol.i.laugh@gmail.com>
2013-03-19 21:59:59 -0700

    modifying for complete solution

--- problem2.py
+++ problem2.py
@@ -1,17 +1,23 @@
 # Project Euler Solution for Problem 2
 
 
-LIMIT = 10
-#LIMIT = 4000000;
+#LIMIT = 10
+LIMIT = 4000000
 
 i = 1 # starting number
 j = 0 
 
-for q in range(LIMIT + 1):
+sum = 0 # sum / answer
+
+while i < LIMIT:
     temp = i
     i = i + j
     j = temp
-    print i
+
+    if i % 2 == 0 and i <= LIMIT:  # check if even
+        sum += i
+
+print sum
 
 
 
