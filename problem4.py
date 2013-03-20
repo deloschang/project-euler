4762b2c4d56334eda6f9e4f3267ed673ae0bd5a9
Delos Chang <lol.i.laugh@gmail.com>
2013-03-19 23:19:51 -0700

    check for palindrome

--- problem4.py
+++ problem4.py
@@ -1,1 +1,32 @@
 # Problem 4 Solution
+
+def is_palindrome(n):
+    n = str(n)  # convert to string
+
+    i, j = 0, -1
+
+    head = n[i]
+    tail = n[j]
+
+    while i < len(n):
+
+        if not iterate_letters(head, tail):
+            return False
+
+        i += 1
+        j -= 1
+
+    return True
+
+
+def iterate_letters(head, tail):
+    return head == tail
+
+
+print is_palindrome(9009) # test even case
+print is_palindrome(50) # test false case
+print is_palindrome(5) # test edge case
+print is_palindrome(91019) # test odd case
+
+
+
